ad1cc73f0dcc8207cb80f52c109d2fcf6da291e9
Yani Venelinov Stratev <veneliy@basfad.basf.net>
2024-01-18 14:31:49 +0100

    fixing main.tf for aws 4

--- microservices/configuration/iaac/aws/kubernetes/main.tf
+++ microservices/configuration/iaac/aws/kubernetes/main.tf
@@ -60,10 +60,10 @@ resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "my-cluster-node-group"
   node_role_arn   = aws_iam_role.eks_node.arn
-  subnet_ids      = [data.aws_subnets.default.ids[0]]
+  subnet_ids      = toset([data.aws_subnets.default.ids[0],data.aws_subnets.default.ids[1]])
 
   scaling_config {
-    desired_size = 2
+    desired_size = 5
     max_size     = 3
     min_size     = 1
   }
