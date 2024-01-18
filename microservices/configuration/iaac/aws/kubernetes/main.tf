223993bdc9a82c0c58ba16a0243eb8cdf8f38f48
Yani Venelinov Stratev <veneliy@basfad.basf.net>
2024-01-18 12:17:18 +0100

    fixing main.tf for aws

--- microservices/configuration/iaac/aws/kubernetes/main.tf
+++ microservices/configuration/iaac/aws/kubernetes/main.tf
@@ -1,13 +1,17 @@
 # aws --version
-# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
+# aws eks --region us-east-1 update-kubeconfig --name yvenstr-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
+# terraform-backend-state-yvenstr-123
 # AKIA4AHVNOD7OOO6T4KI
+
+## arn:aws:s3:::terraform-backend-state-yvenstr-123
+##   jS7nwFuoiqhHHf1Iffx7HUQoDV3f9orazDEjTrVz
+
 
 
 terraform {
   backend "s3" {
-    bucket = "mybucket" # Will be overridden from build
+    bucket = "mybucket"       # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
     region = "us-east-1"
   }
@@ -28,32 +32,32 @@ provider "kubernetes" {
   version                = "~> 2.12"
 }
 
-module "in28minutes-cluster" {
+module "yvenstr-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "in28minutes-cluster"
+  cluster_name    = "yvenstr-cluster"
   cluster_version = "1.14"
-  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
+  subnets         = ["subnet-03a58d7490c6ee564", "subnet-0e256abab875c99f6"]
   #subnets = data.aws_subnet_ids.subnets.ids
-  vpc_id          = aws_default_vpc.default.id
+  vpc_id = aws_default_vpc.default.id
 
   #vpc_id         = "vpc-1234556abcdef"
 
   node_groups = [
     {
-      instance_type = "t2.micro"
-      max_capacity  = 5
-      desired_capacity = 3
-      min_capacity  = 3
+      instance_type    = "t2.micro"
+      max_capacity     = 3
+      desired_capacity = 2
+      min_capacity     = 1
     }
   ]
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.yvenstr-cluster.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.yvenstr-cluster.cluster_id
 }
 
 
@@ -78,5 +82,5 @@ resource "kubernetes_cluster_role_binding" "example" {
 
 # Needed to set the default region
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
